43f126cda39ddcb99f0ac91b89d90e2af9ed6008
Shawn FH <dreadpirateshawn@gmail.com>
2019-03-09 00:00:15 -0800

    Misc refactor and fixes.

--- want_to_read_list.py
+++ want_to_read_list.py
@@ -3,6 +3,45 @@ import pygsheets
 from goodreads import client
 from goodreads.book import GoodreadsBook
 
+def get_series_name_and_pos(book):
+    series_title = None
+    series_pos = None
+    #   OrderedDict([
+    #    ('series_work', OrderedDict([
+    #      ('id', '841082'),
+    #      ('user_position', '1'),
+    #      ('series', OrderedDict([
+    #        ('id', '166200'),
+    #        ('title', 'Terra Ignota'),
+    #        ('description', 'Science fiction often has...'),
+    #        ('note', None),
+    #        ('series_works_count', '4'),
+    #        ('primary_work_count', '4'),
+    #        ('numbered', 'true')]))]))])
+
+    # Check for absent, or for multiple series (e.g. Swamp Thing comics)
+    if not book.series_works:
+        return None, None
+    series_work = book.series_works.get("series_work", {})
+    if not series_work:
+        return None, None
+    if isinstance(series_work, list):
+        return "(multiple series)", None
+
+    # Otherwise process as a dict
+    series_title = series_work.get("series", {}).get("title", None)
+    series_pos = "{pos} of {count} or {count2}".format(
+        pos = series_work.get("user_position", "?"),
+        count = series_work.get("series", {}).get("series_works_count", "?"),
+        count2 = series_work.get("series", {}).get("primary_work_count", "?"))
+    return series_title, series_pos
+
+def has_genre(shelves, *targets):
+    for target in targets:
+        if any([x for x in shelves if target in x]):
+            return "x"
+    return None
+
 def main(args):
     # Goodreads
     gc = client.GoodreadsClient(args.goodreads_key, args.goodreads_secret)
@@ -21,8 +60,9 @@ def main(args):
         "# Ratings",
         "Fantasy",
         "Sci-fi",
-        "Post-apocalyptic",
+        "Post-apoc",
         "Horror",
+        "Comics",
         "Series",
         "Series #",
         "Series Works",
@@ -32,39 +72,21 @@ def main(args):
     values = []
     for book in books:
         # Indicate progress...
-        print("[%s/%s] %s" % (itercount, len(books), book.title))
+        print("[%s/%s] %s ~ %s" % (itercount, len(books), book.gid, book.title))
         # Fetch full version of the book here, to populate things like popular shelves.
         book = gc.book(book_id=book.gid)
         # Original title
         original_title = book.work.get("original_title", None) #if book.work else None
         # Series info
-        if book.series_works:
-            # OrderedDict([
-            #  ('series_work', OrderedDict([
-            #    ('id', '841082'),
-            #    ('user_position', '1'),
-            #    ('series', OrderedDict([
-            #      ('id', '166200'),
-            #      ('title', 'Terra Ignota'),
-            #      ('description', 'Science fiction often has...'),
-            #      ('note', None),
-            #      ('series_works_count', '4'),
-            #      ('primary_work_count', '4'),
-            #      ('numbered', 'true')]))]))])
-            series_title = book.series_works.get("series_work", {}).get("series", {}).get("title", None)
-            series_pos = "{pos} of {count} or {count2}".format(
-                pos = book.series_works.get("series_work", {}).get("user_position", "?"),
-                count = book.series_works.get("series_work", {}).get("series", {}).get("series_works_count", "?"),
-                count2 = book.series_works.get("series_work", {}).get("series", {}).get("primary_work_count", "?"))
-        else:
-            series_title = series_pos = None
+        series_title, series_pos = get_series_name_and_pos(book)
         # Genre flags?
-        fantasy = "x" if any([x for x in book.popular_shelves if "fantasy" in x]) else ""
-        scifi = "x" if any([x for x in book.popular_shelves if "sci-fi" in x or "science-fiction" in x]) else ""
-        postapoc = "x" if any([x for x in book.popular_shelves if "post-apocalyptic" in x]) else ""
-        horror = "x" if any([x for x in book.popular_shelves if "horror" in x]) else ""
+        fantasy = has_genre(book.popular_shelves, "fantasy")
+        scifi = has_genre(book.popular_shelves, "sci-fi", "science-fiction")
+        postapoc = has_genre(book.popular_shelves, "post-apocalyptic")
+        horror = has_genre(book.popular_shelves, "horror")
+        comics = has_genre(book.popular_shelves, "comics", "graphic-novels")
         # Add to values array for spreadsheet.
-        values.append([
+        values.append([str(x) if x else None for x in [
             book.gid,
             ', '.join([author.name for author in book.authors]),
             book.title,
@@ -77,11 +99,12 @@ def main(args):
             scifi,
             postapoc,
             horror,
+            comics,
             series_title,
             series_pos,
-            str(book.series_works),
-            str(book.popular_shelves),
-        ])
+            book.series_works,
+            book.popular_shelves,
+        ]])
         itercount += 1
 
     # Sheets
